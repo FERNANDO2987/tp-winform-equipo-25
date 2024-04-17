1985f95f80d3903dbd3ba07e6af675e23a179065
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-17 00:00:42 -0300

    Add: Nuevas librerias

---
+++ UTN_ABM/UTNBusiness/Module/ArticulosModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTNBusiness.Interfaces;
+using UTNData.Interfaces;
+using UTNData.Models;
+
+namespace UTNBusiness.Module
+{
+    public class ArticulosModule : IArticulosModule
+    {
+        private readonly IArticulosRepository _articulosRepository;
+
+        public ArticulosModule(IArticulosRepository articulosRepository)
+        {
+            _articulosRepository = articulosRepository;
+        }
+        public Task<List<Articulos>> ObtenerArticulos(int id)
+        {
+            var result = _articulosRepository.ObtenerArticulos(id);
+
+            throw new NotImplementedException();
+        }
+    }
+}
