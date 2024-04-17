52b24f7ff46fc1405f1d03d0c8b676d43dac3500
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-16 23:59:57 -0300

    Add: Interfaz al Business

---
+++ UTN_ABM/UTNData/Interfaces/IArticulosRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTNData.Models;
+
+namespace UTNData.Interfaces
+{
+     public interface IArticulosRepository
+    {
+        Task<List<Articulos>> ObtenerArticulos(int id);
+    }
+}
