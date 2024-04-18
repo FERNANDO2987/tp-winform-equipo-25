cf834ab3c01f3e9fd9ef6a9f43fe5c74d51498ab
diazemilianoariel <emiliano.diaz@alumnos.frgp.utn.edu.ar>
2024-04-18 18:48:33 -0300

    se agrego la clase de Reopsitorio de Categorias.

--- UTN_ABM/UTNData/Entity/Imagenes.cs
+++ UTN_ABM/UTNData/Entity/Imagenes.cs
@@ -14,6 +14,7 @@ namespace UTNData.Entity
         public string imagenURL{ get; set; }
 
 
+
         // hecho. 
     }
 }
---
+++ UTN_ABM/UTNData/Repository/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTNData.Repository
+{
+    internal class CategoryRepository
+    {
+        //  Implementar modelos para interactuar con la base de datos
+        // queda por realizar la implementacion de los metodos para interactuar con la base de datos
+     
+    }
+}
