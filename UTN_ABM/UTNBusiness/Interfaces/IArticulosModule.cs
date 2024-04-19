ee24bd3e5f8ffc585cf72398221366d4f5604167
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-19 20:24:22 -0300

    Add: se agrega nueva Interfaz

--- UTN_ABM/UTNBusiness/Interfaces/IArticulosModule.cs
+++ UTN_ABM/UTNBusiness/Interfaces/IArticulosModule.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using UTNData.Models;
+using UTNBusiness.Models;
+
 
 namespace UTNBusiness.Interfaces
 {
      public interface IArticulosModule
     {
-        Task<List<Articulos>> ObtenerArticulos(int id);
+        Task<List<Articulos>> ObtenerArticulos(int idArticulo);
+        Task<Articulos> AgregarArticulos(Articulos articulos);
+        Task<Articulos> ModificarArticulos(Articulos articulos);
+        Task<Articulos> EliminarArticulos(int id);
 
     }
 }
