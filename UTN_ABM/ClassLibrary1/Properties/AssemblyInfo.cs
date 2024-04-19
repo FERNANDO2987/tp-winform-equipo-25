e2669094104f3d698bb3ddf0d5c8288b7aa7b190
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-19 20:24:50 -0300

    Add: se agrega nuevas clases al Business

--- UTN_ABM/ClassLibrary1/Properties/AssemblyInfo.cs
+++ UTN_ABM/ClassLibrary1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("UTNData")]
+[assembly: AssemblyTitle("ClassLibrary1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UTNData")]
+[assembly: AssemblyProduct("ClassLibrary1")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
-[assembly: Guid("be516170-7de9-45dc-b979-9ae8726c4331")]
+[assembly: Guid("eef944c9-5faf-44fa-adf1-cc639eb4f52a")]
 
 // La información de versión de un ensamblado consta de los cuatro valores siguientes:
 //
--- UTN_ABM/UTNBusiness/Module/ArticulosModule.cs
+++ UTN_ABM/UTNBusiness/Module/ArticulosModule.cs
@@ -1,27 +1,117 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UTNBusiness.Interfaces;
-using UTNData.Interfaces;
-using UTNData.Models;
+using UTNBusiness.Models;
+
+
+
 
 namespace UTNBusiness.Module
 {
     public class ArticulosModule : IArticulosModule
     {
-        private readonly IArticulosRepository _articulosRepository;
+        private readonly string sqlconString;
+        private readonly IConfiguration _configuration;
 
-        public ArticulosModule(IArticulosRepository articulosRepository)
+        public ArticulosModule(IConfiguration configuration)
         {
-            _articulosRepository = articulosRepository;
+            _configuration = configuration;
+            sqlconString = _configuration["ConnectionStrings:"];
         }
-        public Task<List<Articulos>> ObtenerArticulos(int id)
-        {
-            var result = _articulosRepository.ObtenerArticulos(id);
 
+        public Task<Articulos> AgregarArticulos(Articulos articulos)
+        {
             throw new NotImplementedException();
         }
+
+        public Task<Articulos> EliminarArticulos(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Articulos> ModificarArticulos(Articulos articulos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Articulos>> ObtenerArticulos(int id)
+        {
+            // Crea una nueva lista para almacenar los objetos Articulos.
+            var lista = new List<Articulos>();
+
+            // Crea una nueva conexión a la base de datos utilizando la cadena de conexión sqlconString.
+            using var conn = new SqlConnection(sqlconString);
+
+            // Crea un nuevo comando SQL para ejecutar el procedimiento almacenado ObtenerArticulos.
+            using SqlCommand command = new SqlCommand("ObtenerArticulos", conn)
+            {
+                // Establece el tipo de comando como procedimiento almacenado.
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+
+            // Abre la conexión a la base de datos.
+            conn.Open();
+
+            // Agrega un parámetro al comando SQL para pasar el ID de los artículos.
+            command.Parameters.Add(new SqlParameter("@idArticulos", id));
+
+            // Ejecuta y lee los resultados de la consulta SQL
+            var reader = command.ExecuteReader();
+
+            try
+            {
+                // Itera a través de cada fila devuelta por el lector de datos.
+                while (reader.Read())
+                {
+                    // Crea un nuevo objeto Articulos y lo agrega a la lista.
+                    lista.Add(new Articulos
+                    {
+                        // Asigna valores a las propiedades del objeto Articulos utilizando los datos leídos.
+                        Id = reader.GetInt32(0),
+                        Codigo = reader.GetString(1),
+                        Nombre = reader.GetString(2),
+                        Descripcion = reader.GetString(3),
+                        // Crea un nuevo objeto Marca y asigna el ID leído.
+                        Marca = new Marca()
+                        {
+                            Id = reader.GetInt32(4)
+                        },
+                        // Crea un nuevo objeto Categoria y asigna el ID leído.
+                        Categoria = new Categoria()
+                        {
+                            Id = reader.GetInt32(5)
+                        },
+                        // Asigna el precio.
+                        Precio = reader.GetDecimal(6)
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                // Captura cualquier excepción que ocurra durante el proceso de lectura de datos.
+                var error = "Error parseando datos de SQL: " + ex.Message;
+
+                // Devuelve una nueva lista vacía de objetos Articulos.
+                return new List<Articulos>();
+            }
+            finally
+            {
+                // Cierra el lector de datos para liberar recursos.
+                reader.Close();
+                // Libera los recursos asociados con el comando SQL.
+                command.Dispose();
+                // Cierra la conexión a la base de datos para liberar recursos y finalizar la conexión.
+                conn.Close();
+            }
+
+            // Devuelve la lista de objetos Articulos que se han leído de la base de datos.
+            return lista;
+        }
+
     }
 }
