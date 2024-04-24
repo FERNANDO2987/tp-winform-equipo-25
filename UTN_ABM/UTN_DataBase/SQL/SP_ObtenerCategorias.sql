b24a2b527079b56737e2dcd1a2f08564d97da7cf
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-24 10:58:41 -0300

    Add: se agrega tablas y SP

--- UTN_ABM/UTN_DataBase/SQL/SP_ObtenerCategorias.sql
+++ UTN_ABM/UTN_DataBase/SQL/SP_ObtenerCategorias.sql
@@ -1,1 +1,18 @@
-﻿
+﻿USE [CATALOGO_P3_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[ObtenerCategorias]    Script Date: 24/04/2024 10:57:26 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ObtenerCategorias]
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    SELECT [Id], [Descripcion]
+    FROM [dbo].[CATEGORIAS];
+END;
+
+
