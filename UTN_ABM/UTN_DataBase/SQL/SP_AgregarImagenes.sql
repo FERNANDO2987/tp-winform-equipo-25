f4adc0bbf3ee4d88c2d4442cd848088e4deffe6b
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-27 10:04:32 -0300

    Add: se agrega nuevo stored Procedure para guardar las imagens

---
+++ UTN_ABM/UTN_DataBase/SQL/SP_AgregarImagenes.sql
@@ -0,0 +1,32 @@
+﻿USE [CATALOGO_P3_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[AgregarImagenes]    Script Date: 27/04/2024 10:03:17 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[AgregarImagenes]
+  (  @Id INT,
+    @IdArticulo INT,
+    @ImagenUrl VARCHAR(1000)
+   
+  ) 
+AS BEGIN
+
+     	if (EXISTS (SELECT * FROM [dbo].[IMAGENES] WHERE Id = @Id ))
+		UPDATE [dbo].[IMAGENES] SET IdArticulo = @IdArticulo,
+		                             ImagenUrl = @ImagenUrl
+									 
+									 WHERE Id = @Id
+		else
+		    BEGIN
+
+INSERT INTO [dbo].[IMAGENES]
+VALUES (@IdArticulo, @ImagenUrl)
+
+       SELECT SCOPE_IDENTITY()
+	   END
+
+END
+
