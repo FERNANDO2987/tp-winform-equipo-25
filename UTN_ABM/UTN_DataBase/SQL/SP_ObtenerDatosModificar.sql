cf05a4cf805f1c6dcf8d72fbdd16c562dad79fb9
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-27 20:26:49 -0300

    Add: se agrega Nuevo Stored Procedure para obtener datos a Modificar

---
+++ UTN_ABM/UTN_DataBase/SQL/SP_ObtenerDatosModificar.sql
@@ -0,0 +1,45 @@
+﻿USE [CATALOGO_P3_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[ListarDetalle]    Script Date: 27/04/2024 20:24:16 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ObtenerDatosModificar]
+@criterio varchar(50) = NULL
+AS
+BEGIN
+     SET NOCOUNT ON;
+
+	 SELECT
+	 A.Id,
+	 A.Codigo,
+	 A.Nombre,
+	 A.Descripcion,
+	 COALESCE(M.Descripcion, 'null') AS Marca,
+	 COALESCE(C.Descripcion, 'null') AS Categoria,
+	 A.Precio,
+	 COALESCE(I.ImagenUrl, 'null') AS ImagenUrl
+	 FROM 
+	 ARTICULOS A
+	 LEFT JOIN
+	 MARCAS M ON A.IdMarca = M.Id
+	 LEFT JOIN
+	 CATEGORIAS C ON A.IdCategoria = C.Id
+	 LEFT JOIN 
+	 IMAGENES I ON A.Id = I.IdArticulo
+	 WHERE 
+	 (
+	     A.Nombre LIKE '%' + @criterio + '%'
+	     OR
+	     COALESCE(M.Descripcion, 'null') LIKE '%' + @criterio + '%'
+	     OR
+	     COALESCE(C.Descripcion, 'null') LIKE '%' + @criterio + '%'
+	     OR
+	     CONVERT(varchar(50), A.Precio) LIKE '%' + @criterio + '%'
+	     OR
+	     COALESCE(I.ImagenUrl, 'null') LIKE '%' + @criterio + '%'
+	 );
+
+END;
