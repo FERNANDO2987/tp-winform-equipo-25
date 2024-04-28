99989ce16c84fd5cca922fc2334d74cc60dcfe93
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-27 23:06:34 -0300

    Add: Nuevos Metodos de obtener datos para modificar articulo

--- UTN_ABM/UTNBusiness/Models/Detalle.cs
+++ UTN_ABM/UTNBusiness/Models/Detalle.cs
@@ -17,5 +17,7 @@ namespace UTNBusiness.Models
         public string Categoria { get; set; }
         public decimal Precio { get; set; }
         public string ImagenUrl { get; set; }
+        public int IdMarca { get; set; }
+        public int IdCategoria { get; set; }    
     }
 }
--- UTN_ABM/UTNBusiness/Module/ArticulosModule.cs
+++ UTN_ABM/UTNBusiness/Module/ArticulosModule.cs
@@ -335,10 +335,78 @@ namespace UTNBusiness.Module
             return lista;
         }
 
-        public Task<Articulos> ModificarArticulos(Articulos articulos)
+        public async Task<Articulos> ModificarArticulos(Articulos obj)
         {
-            // falta hacer
-            throw new NotImplementedException();
+            var conn = new SqlConnection(sqlconString);
+
+            SqlCommand command = new SqlCommand("ModificarArticulo", conn)
+            {
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+            conn.Open();
+
+
+            try
+            {
+                command.Parameters.Add(new SqlParameter("@Id", obj.Id));
+                command.Parameters.Add(new SqlParameter("@Codigo", obj.Codigo));
+                command.Parameters.Add(new SqlParameter("@Nombre", obj.Nombre));
+                command.Parameters.Add(new SqlParameter("@Descripcion", obj.Descripcion));
+                command.Parameters.Add(new SqlParameter("@IdMarca", obj.IdMarca));
+                command.Parameters.Add(new SqlParameter("@IdCategoria", obj.IdCategoria));
+                command.Parameters.Add(new SqlParameter("@Precio", obj.Precio));
+
+                var reader = command.ExecuteReader();
+
+
+                try
+                {
+                    while (reader.Read())
+                    {
+                        /// Validacion para Identity, devuelve el valor de las tres formas
+                        if (reader[0].GetType() == typeof(int))
+                        {
+                            obj.Id = reader.GetInt32(0);
+                        }
+                        else
+                        if (reader[0].GetType() == typeof(decimal))
+                        {
+                            obj.Id = (int)reader.GetDecimal(0);
+                        }
+                        else
+                        {
+                            obj.Id = (int)reader.GetInt64(0);
+
+                        }
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    var error = "Error parseando datos de sql: " + ex.Message;
+                    return null;
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                var error = "Error parseando datos de sql: " + ex.Message;
+                return null;
+
+            }
+            finally
+            {
+
+                conn.Close();
+
+
+            }
+
+
+            return obj;
+
         }
 
         public async Task<List<Articulos>> ObtenerArticulos(int id)
@@ -406,5 +474,72 @@ namespace UTNBusiness.Module
             return lista;
         }
 
+        public async Task<List<Detalle>> ObtenerDatosModificar(string criterio)
+        {
+            // Crea una nueva lista para almacenar los objetos Articulos.
+            var lista = new List<Detalle>();
+
+            // Crea una nueva conexion a la base de datos utilizando la cadena de conexion sqlconString.
+            var conn = new SqlConnection(sqlconString);
+
+            // Crea un nuevo comando SQL para ejecutar el procedimiento almacenado ObtenerArticulos.
+            SqlCommand command = new SqlCommand("ObtenerDatosModificar", conn)
+            {
+                // Establece el tipo de comando como procedimiento almacenado.
+                CommandType = System.Data.CommandType.StoredProcedure
+            };
+
+            // Agrega un parometro al comando SQL para pasar el ID de los artoculos.
+            command.Parameters.Add(new SqlParameter("@criterio", criterio));
+
+            // Abre la conexion a la base de datos.
+            conn.Open();
+
+            // Ejecuta y lee los resultados de la consulta SQL
+            var reader = command.ExecuteReader();
+
+            try
+            {
+                // Itera a traves de cada fila devuelta por el lector de datos.
+                while (reader.Read())
+                {
+                    lista.Add(new Detalle
+                    {
+                        Id = Convert.ToInt32(reader["Id"]),
+                        Codigo = Convert.ToString(reader["Codigo"]),
+                        Nombre = Convert.ToString(reader["Nombre"]),
+                        Descripcion = Convert.ToString(reader["Descripcion"]),
+                        Marca = Convert.ToString(reader["Marca"]),
+                        Categoria = Convert.ToString(reader["Categoria"]),
+                        Precio = Convert.ToDecimal(reader["Precio"]),
+                        ImagenUrl = Convert.ToString(reader["ImagenUrl"]),
+                        IdMarca = Convert.ToInt32(reader["Id"]),
+                        IdCategoria = Convert.ToInt32(reader["Id"]),
+                    });
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                // Captura cualquier excepcion que ocurra durante el proceso de lectura de datos.
+                var error = "Error parseando datos de SQL: " + ex.Message;
+
+                // Devuelve una nueva lista vacia de objetos Articulos.
+                return new List<Detalle>();
+            }
+            finally
+            {
+                // Cierra el lector de datos para liberar recursos.
+                reader.Close();
+                // Libera los recursos asociados con el comando SQL.
+                command.Dispose();
+                // Cierra la conexion a la base de datos para liberar recursos y finalizar la conexion.
+                conn.Close();
+            }
+
+            // Devuelve la lista de objetos Marca que se han leido de la base de datos.
+            return lista;
+        }
     }
 }
