62a4a015e4521c2e62ed325f5ddaf8fac0d90b4f
MatiasRamirez315 <matiirami315@gmail.com>
2024-04-28 00:02:26 -0300

    ADD: se agrega ListarArticulos a la base de datos

---
+++ UTN_ABM/UTN_DataBase/SQL/SP_ListarArticulos.sql
@@ -0,0 +1,15 @@
+﻿USE [CATALOGO_P3_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[ListarArticulos]    Script Date: 27/04/2024 11:55:34 p.m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER PROCEDURE [dbo].[ListarArticulos]
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    SELECT [Id],[Codigo],[Nombre],[Descripcion],[IdMarca],[IdCategoria],[Precio]
+    FROM [dbo].[ARTICULOS];
+END;
