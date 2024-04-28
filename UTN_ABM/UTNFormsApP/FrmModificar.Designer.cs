c5891054911bfaedf949a33c32d015d4a23e0208
Fernando2987 <fernandopalacios51@gmail.com>
2024-04-27 23:07:56 -0300

    Change: se modifica Formulario Modificar

--- UTN_ABM/UTNFormsApP/FrmModificar.Designer.cs
+++ UTN_ABM/UTNFormsApP/FrmModificar.Designer.cs
@@ -29,6 +29,15 @@
         private void InitializeComponent()
         {
             this.label_Modificar = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Buscar = new System.Windows.Forms.Button();
+            this.text_Modificar = new System.Windows.Forms.TextBox();
+            this.Modificar = new System.Windows.Forms.GroupBox();
+            this.dataGridView_Modificar = new System.Windows.Forms.DataGridView();
+            this.btn_ModificarArticulo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.Modificar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Modificar)).BeginInit();
             this.SuspendLayout();
             // 
             // label_Modificar
@@ -41,16 +50,95 @@
             this.label_Modificar.TabIndex = 1;
             this.label_Modificar.Text = "Modificar Articulo";
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Buscar);
+            this.groupBox1.Controls.Add(this.text_Modificar);
+            this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.groupBox1.Location = new System.Drawing.Point(18, 88);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(678, 66);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Buscar Articulo a Modificar";
+            // 
+            // btn_Buscar
+            // 
+            this.btn_Buscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.btn_Buscar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Buscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Buscar.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.btn_Buscar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_Buscar.Location = new System.Drawing.Point(545, 23);
+            this.btn_Buscar.Name = "btn_Buscar";
+            this.btn_Buscar.Size = new System.Drawing.Size(114, 33);
+            this.btn_Buscar.TabIndex = 16;
+            this.btn_Buscar.Text = "Buscar";
+            this.btn_Buscar.UseVisualStyleBackColor = false;
+            this.btn_Buscar.Click += new System.EventHandler(this.btn_Buscar_Click);
+            // 
+            // text_Modificar
+            // 
+            this.text_Modificar.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.text_Modificar.Location = new System.Drawing.Point(18, 29);
+            this.text_Modificar.Name = "text_Modificar";
+            this.text_Modificar.Size = new System.Drawing.Size(506, 23);
+            this.text_Modificar.TabIndex = 0;
+            // 
+            // Modificar
+            // 
+            this.Modificar.Controls.Add(this.dataGridView_Modificar);
+            this.Modificar.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.Modificar.Location = new System.Drawing.Point(18, 172);
+            this.Modificar.Name = "Modificar";
+            this.Modificar.Size = new System.Drawing.Size(684, 351);
+            this.Modificar.TabIndex = 7;
+            this.Modificar.TabStop = false;
+            this.Modificar.Text = "Modificar";
+            // 
+            // dataGridView_Modificar
+            // 
+            this.dataGridView_Modificar.AllowUserToAddRows = false;
+            this.dataGridView_Modificar.AllowUserToDeleteRows = false;
+            this.dataGridView_Modificar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Modificar.Location = new System.Drawing.Point(6, 19);
+            this.dataGridView_Modificar.Name = "dataGridView_Modificar";
+            this.dataGridView_Modificar.Size = new System.Drawing.Size(672, 299);
+            this.dataGridView_Modificar.TabIndex = 0;
+            this.dataGridView_Modificar.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridModificar_CellEndEdit);
+            // 
+            // btn_ModificarArticulo
+            // 
+            this.btn_ModificarArticulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.btn_ModificarArticulo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ModificarArticulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_ModificarArticulo.Font = new System.Drawing.Font("Century Gothic", 11.25F);
+            this.btn_ModificarArticulo.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn_ModificarArticulo.Location = new System.Drawing.Point(232, 546);
+            this.btn_ModificarArticulo.Name = "btn_ModificarArticulo";
+            this.btn_ModificarArticulo.Size = new System.Drawing.Size(174, 33);
+            this.btn_ModificarArticulo.TabIndex = 19;
+            this.btn_ModificarArticulo.Text = "Modificar Articulo";
+            this.btn_ModificarArticulo.UseVisualStyleBackColor = false;
+            this.btn_ModificarArticulo.Click += new System.EventHandler(this.btn_ModificarArticulo_Click);
+            // 
             // FrmModificar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(708, 600);
+            this.Controls.Add(this.btn_ModificarArticulo);
+            this.Controls.Add(this.Modificar);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label_Modificar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmModificar";
             this.Text = "FrmModificar";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.Modificar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Modificar)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,5 +147,11 @@
         #endregion
 
         private System.Windows.Forms.Label label_Modificar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Buscar;
+        private System.Windows.Forms.TextBox text_Modificar;
+        private System.Windows.Forms.GroupBox Modificar;
+        private System.Windows.Forms.DataGridView dataGridView_Modificar;
+        private System.Windows.Forms.Button btn_ModificarArticulo;
     }
 }
--- UTN_ABM/UTNFormsApP/FrmModificar.cs
+++ UTN_ABM/UTNFormsApP/FrmModificar.cs
@@ -1,20 +1,244 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UTNBusiness.Models;
+using UTNBusiness.Module;
 
 namespace UTNFormsApP
 {
     public partial class FrmModificar : Form
     {
+
+        private DataTable dataTable;
         public FrmModificar()
         {
             InitializeComponent();
+
+            InitializeTextBoxes();
+
+           
+
+        }
+
+        private void InitializeTextBoxes()
+        {
+            SetPlaceholder(text_Modificar, "Ingrese cualquier criterio para Buscar el Articulo a Modificar");
+
+
+        }
+
+        private void SetPlaceholder(TextBox textBox, string placeholder)
+        {
+            textBox.ForeColor = SystemColors.GrayText;
+            textBox.Text = placeholder;
+            textBox.Enter += (sender, e) =>
+            {
+                if (textBox.Text == placeholder)
+                {
+                    textBox.Text = "";
+                    textBox.ForeColor = SystemColors.WindowText;
+                }
+            };
+            textBox.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    textBox.ForeColor = SystemColors.GrayText;
+                    textBox.Text = placeholder;
+                }
+            };
+        }
+
+
+        private async void InicializarSelectArticulos()
+        {
+            string connectionString = ConfigurationManager.AppSettings["ConnectionStringUTN"];
+
+            ArticulosModule result = new ArticulosModule(connectionString);
+            try
+            {
+                var criterio = text_Modificar.Text;
+                if (!string.IsNullOrWhiteSpace(criterio))
+                {
+
+
+                    var articulos = await result.ObtenerDatosModificar(criterio);
+
+                    if (articulos.Any()) // Verifica si se encontraron artículos
+                    {
+                        // Agrega cada artículo individualmente al DataGridView
+                        dataGridView_Modificar.DataSource = articulos;
+
+                        dataGridView_Modificar.Columns["ID"].Visible = false;
+                        dataGridView_Modificar.Columns["ID"].ReadOnly = true;
+                        dataGridView_Modificar.Columns["ID"].DefaultCellStyle.SelectionBackColor = dataGridView_Modificar.DefaultCellStyle.BackColor;
+
+                        dataGridView_Modificar.Columns["IdMarca"].ReadOnly = true;
+                        dataGridView_Modificar.Columns["IdMarca"].DefaultCellStyle.SelectionBackColor = dataGridView_Modificar.DefaultCellStyle.BackColor;
+                    }
+                    else
+                    {
+                        // Mostrar un mensaje si no se encontraron resultados
+                        MessageBox.Show("Debe agregar un criterio en la búsqueda.", "Sin Resultados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    // Agrega cada artículo individualmente al DataGridView
+
+
+                }
+                else
+                {
+                    // Mostrar un mensaje si el campo está vacío
+                    MessageBox.Show("Debe completar el TextBox antes de realizar la búsqueda.", "Campo Vacío", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+       
+
+        private void LimpiarControles()
+        {
+            // Limpiar TextBox
+            text_Modificar.Text = "";
+
+        }
+
+        private async void btn_ModificarArticulo_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.AppSettings["ConnectionStringUTN"];
+
+            ArticulosModule result = new ArticulosModule(connectionString);
+            ImagenesModule imagenesModule = new ImagenesModule(connectionString);
+
+            try
+            {
+                // Recorrer las filas del DataGridView
+                foreach (DataGridViewRow row in dataGridView_Modificar.Rows)
+                {
+                    // Obtener el identificador único del artículo
+                    int idArticulo = Convert.ToInt32(row.Cells["ID"].Value); // Ajusta "ID" al nombre de la columna que contiene el ID del artículo
+
+                    // Obtener el nuevo valor modificado para la Columna Codigo
+                    string nuevoCodigo = row.Cells["Codigo"].Value.ToString();
+
+                    // Obtener el nuevo valor modificado para la Columna Nombre
+                    string nuevoValor = row.Cells["Nombre"].Value.ToString();
+
+                    // Obtener el nuevo valor modificado para la Columna Descripcion
+                    string nuevoDescripcion = row.Cells["Descripcion"].Value.ToString();
+
+
+                    // Obtener el nuevo valor modificado para la Columna Descripcion de la Marca
+                    string nuevaMarca = row.Cells["Marca"].Value.ToString();
+
+                    // Obtener el nuevo valor modificado para la Columna Descripcion de la Categoria
+                    string nuevaCategoria = row.Cells["Categoria"].Value.ToString();
+
+
+                    // Obtener el nuevo valor modificado para la Columna Precio
+                    decimal nuevoPrecio = Convert.ToDecimal(row.Cells["Precio"].Value);
+
+                    // Obtener el nuevo valor modificado para la Columna la Imagen
+                    string nuevaImagen  = row.Cells["ImagenUrl"].Value.ToString();
+
+
+                    // Actualizar el artículo en la base de datos
+                    var articulo = new Articulos
+                    {
+                        Id = idArticulo,
+                        Codigo = nuevoCodigo,
+                        Nombre = nuevoValor,
+                        Descripcion = nuevoDescripcion,
+                       IdMarca=1,
+                       IdCategoria=1,
+
+                        Precio = nuevoPrecio
+
+
+                    };
+
+                    await result.ModificarArticulos(articulo);
+
+                    var imagen = new Imagenes
+                    {
+                        Id=1,
+                        IdArticulo=idArticulo,
+                        ImagenURL=""
+
+
+                    };
+
+                    //Cambiar  nombre de Metodo
+                    await imagenesModule.AgregarImagen(imagen);
+
+
+               
+
+                }
+
+                // Mostrar un mensaje de éxito al usuario
+                MessageBox.Show("Los cambios se han guardado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier error que ocurra durante el proceso de guardado
+                MessageBox.Show("Error al guardar los cambios: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            // Limpiar el DataGridView asignándole null como origen de datos
+            dataGridView_Modificar.DataSource = null;
+        }
+
+   
+
+        private void dataGridModificar_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+
+            // Verificar si la celda editada no es la columna de ID
+            if (e.ColumnIndex != 0) // Suponiendo que la columna de ID tiene el índice 0
+            {
+                // Obtener el valor de la celda editada
+                string nuevoValor = dataGridView_Modificar.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+
+                // Modificar el valor de la celda si es necesario
+                // Por ejemplo, puedes convertir el valor a mayúsculas
+                dataGridView_Modificar.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = nuevoValor.ToUpper();
+            }
+        }
+
+        private void btn_Buscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Llamar al método para inicializar la búsqueda de artículos
+                InicializarSelectArticulos();
+                LimpiarControles();
+
+
+            }
+            catch (Exception ex)
+            {
+                // Manejar cualquier error que ocurra durante la inicialización
+                MessageBox.Show("Error al buscar en la base de datos: " + ex.Message);
+            }
         }
     }
 }
