3b861a2469a0f72be1cb04e09de6caa2ae01be85
MatiasRamirez315 <matiirami315@gmail.com>
2024-04-28 18:42:11 -0300

    ADD: se agrega sp_agregarcategoria y sp_modificarImagen

---
+++ UTN_ABM/UTN_DataBase/SQL/SP_AgregarCategoria.sql
@@ -0,0 +1,32 @@
+﻿USE [CATALOGO_P3_DB]
+GO
+/****** Object:  StoredProcedure [dbo].[AgregarArticulo]    Script Date: 28/04/2024 06:35:32 p.m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[AgregarCategoria]
+  (  @Id INT,
+    @Descripcion VARCHAR(50)
+
+  ) 
+AS BEGIN
+
+     	if (EXISTS (SELECT * FROM [dbo].[CATEGORIAS] WHERE Id = @Id ))
+		UPDATE [dbo].[ARTICULOS] SET 
+								Descripcion=@Descripcion
+							
+									 
+									 WHERE Id = @Id
+		else
+		    BEGIN
+
+INSERT INTO [dbo].[CATEGORIAS]
+VALUES (@Descripcion)
+
+       SELECT SCOPE_IDENTITY()
+	   END
+
+END
+
